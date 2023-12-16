f7261b0821189b75c8ce89193171b60bb53302e2
lamela08 <138975073+lamela08@users.noreply.github.com>
2023-12-16 15:14:52 +0500

    Операции nasm

--- ops.asm
+++ ops.asm
@@ -4,33 +4,33 @@
 ;Выравнивание по 4 байта
 
 ;Сложение: op1 (eax) + op2 (ebx) = ecx
-mov	eax, ($reg1/[ds:$op1_name]/$const1)
-mov 	ebx, ($reg2/[ds:$op2_name]/$const2)
+mov	eax, $reg1/[ds:$op1_name]/$const1
+mov 	ebx, $reg2/[ds:$op2_name]/$const2
 mov 	ecx, eax
 add 	ecx, ebx
 
 ;Вычитание: op1 (eax) - op2 (ebx) = ecx
-mov	eax, ($reg1/[ds:$op1_name]/$const1)
-mov 	ebx, ($reg2/[ds:$op2_name]/$const2)
+mov	eax, $reg1/[ds:$op1_name]/$const1
+mov 	ebx, $reg2/[ds:$op2_name]/$const2
 mov	ecx, eax
 sub 	ecx, ebx
 
 ;Деление: op1 (eax) / op2 (ebx) = ecx
-mov	eax, ($reg1/[ds:$op1_name]/$const1)
-mov 	ebx, ($reg2/[ds:$op2_name]/$const2)
+mov	eax, $reg1/[ds:$op1_name]/$const1
+mov 	ebx, $reg2/[ds:$op2_name]/$const2
 idiv	ebx
 mov	ecx, eax
 
 ;Остаток от деления: op1 (eax) % op2 (ebx) = ecx
-mov	eax, ($reg1/[ds:$op1_name]/$const1)
-mov 	ebx, ($reg2/[ds:$op2_name]/$const2)
+mov	eax, $reg1/[ds:$op1_name]/$const1
+mov 	ebx, $reg2/[ds:$op2_name]/$const2
 xor	edx, edx
 idiv	ebx
 mov	ecx, edx
 
 ;Умножение: op1 (eax) * op2 (ebx) = ecx
-mov	eax, ($reg1/[ds:$op1_name]/$const1)
-mov 	ebx, ($reg2/[ds:$op2_name]/$const2)
+mov	eax, $reg1/[ds:$op1_name]/$const1
+mov 	ebx, $reg2/[ds:$op2_name]/$const2
 imul	ebx
 mov	ecx, eax
 
